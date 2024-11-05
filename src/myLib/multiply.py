a3f5efb571e50757a0e89c0bb34b717500769a30
Ignacio Becker <iebecker@g.harvard.edu>
2024-11-05 09:49:50 -0500

    My first file. Multiply function.

---
+++ src/myLib/multiply.py
@@ -0,0 +1,6 @@
+def multiply(a, b):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("Both arguments must be numbers (int or float)")
+    return a * b
+
+
